0a14f96220177179f1db5a4f0ba7d243702ff85b
ao <84378453+aohana-AO@users.noreply.github.com>
2022-03-30 23:09:19 +0900

    Add files via upload

---
+++ aka.java
@@ -0,0 +1,14 @@
+//UTF８に文字コードを変更してみました
+public class aka {
+    public static void main (String[] args) {
+
+        double a;
+        a = 6.5*3.5/2;
+        System.out.println("UTF8 test");
+        System.out.println("三角形の面積計算");
+        System.out.println("底辺：6.5");
+        System.out.println("高さ：3.5");
+        System.out.println("面積は"+a+"です");
+
+        }
+    }
